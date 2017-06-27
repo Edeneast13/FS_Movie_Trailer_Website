56dccabed9bc843ea8e8f9bbdf8d0f4065d4d979
Brian Roper <broper138@gmail.com>
2017-06-26 23:29:28 -0400

    initial commit

---
+++ entertainment_center.py
@@ -0,0 +1,18 @@
+import fresh_tomatoes
+import media
+
+fast_and_furious = media.Movie("The Fast & The Furious",
+                               "https://image.tmdb.org/t/p/original/j9jeJyXWdx1eGVRRwA1drTEjCyV.jpg",
+                               "https://www.youtube.com/watch?v=ZsJz2TJAPjw")
+
+return_of_the_jedi = media.Movie("Star Wars: The Return Of The Jedi",
+                                 "https://image.tmdb.org/t/p/original/tDI5QBosczWGj6w1Xp0Uidr6GKW.jpg",
+                                 "https://www.youtube.com/watch?v=7L8p7_SLzvU")
+
+two_towers = media.Movie("The Lord Of The Rings: The Two Towers",
+                         "https://image.tmdb.org/t/p/original/5o5fv1dHG7vWoH2hmqwihVPBoBm.jpg",
+                         "https://www.youtube.com/watch?v=2dlRvAjU_RI")
+
+movies = [fast_and_furious, return_of_the_jedi, two_towers]
+
+fresh_tomatoes.open_movies_page(movies)
---
+++ media.py
@@ -0,0 +1,8 @@
+import webbrowser
+
+class Movie():
+
+    def __init__(self, title, poster_url, trailer_url):
+        self.title = title
+        self.poster_url = poster_url
+        self.trailer_url = trailer_url
