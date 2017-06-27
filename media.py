1de89f2b4e82a29fa7a23b3333adf63c91e28fbe
Brian Roper <broper138@gmail.com>
2017-06-27 09:41:20 -0400

    made pep8 corrections

--- media.py
+++ media.py
@@ -1,8 +1,26 @@
 import webbrowser
 
-class Movie():
 
-    def __init__(self, title, poster_url, trailer_url):
+"""
+Module to display movie object, attributes and instances
+"""
+
+
+class Movie():
+    """
+    Class object stores movie related information
+    """
+
+    def __init__(self, title, poster_image_url, trailer_youtube_url):
+        """
+        initialize instance of class Movie
+
+        :param movie_title: title
+        :param poster_image_url: poster_image_url
+        :param trailer_youtube_url: trailer_youtube_url
+         """
+
         self.title = title
-        self.poster_url = poster_url
-        self.trailer_url = trailer_url
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
+        
